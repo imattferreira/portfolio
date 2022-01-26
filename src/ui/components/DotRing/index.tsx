c7e109adecea338c34a29c3ea9660d7971220f1b
MattFerreira18 <matheusferreira.dev@gmail.com>
2022-01-26 07:42:24 -0300

    fix: removes cursor when the client is mobile

--- src/ui/components/DotRing/index.tsx
+++ src/ui/components/DotRing/index.tsx
@@ -1,12 +1,22 @@
-import { useMouseContext } from 'app/contexts/MouseContext';
-import { useMousePosition } from 'app/hooks/useMousePosition';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { useMouseContext } from '../../../app/contexts/MouseContext';
+import { useMobileVersion } from '../../../app/hooks/useMobileVersion';
+import { useMousePosition } from '../../../app/hooks/useMousePosition';
 import { Dot, Ring } from './styles';
 
 export function DotRing() {
   const { x, y } = useMousePosition();
   const { cursorType } = useMouseContext();
+  const [customCursorIsActive, setCustomCursorIsActive] = useState(true);
+
+  useEffect(() => {
+    setCustomCursorIsActive(!useMobileVersion());
+  }, [useMobileVersion()]);
+
+  if (!customCursorIsActive) {
+    return null;
+  }
 
   return (
     <>
