f9a2a0a7a5c61e452bbabb958d776558dea04c8e
Matheus Ferreira <mattferreira.dev@protonmail.com>
2023-09-03 00:54:51 -0300

    style: configure eslint

--- src/old/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  productionBrowserSourceMaps: true,
-  swcMinify: true,
-  images: {
-    domains: ['github.com'],
-    minimumCacheTTL: 60 * 60 * 24,
-  }
-}
--- src/old/ui/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as About } from './About';
-export { default as HardSkills } from './HardSkills';
-export { default as Projects } from './Projects';
-export { default as SocialLinks } from './SocialLinks';
-export { default as FeaturedArticles } from './FeaturedArticles';
-export { default as Presentation } from './Presentation';
-export { default as BooksRead } from './BooksRead';
